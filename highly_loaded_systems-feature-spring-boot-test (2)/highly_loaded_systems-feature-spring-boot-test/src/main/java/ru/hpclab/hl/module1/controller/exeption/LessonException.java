b1e808891b5bb2d8c663061abc573be3714416ae
aniuuta <143626703+aniuuta@users.noreply.github.com>
2025-03-14 09:08:06 +0300

    Delete highly_loaded_systems-feature-spring-boot-test (2)/highly_loaded_systems-feature-spring-boot-test directory

--- highly_loaded_systems-feature-spring-boot-test (2)/highly_loaded_systems-feature-spring-boot-test/src/main/java/ru/hpclab/hl/module1/controller/exeption/LessonException.java
+++
@@ -1,8 +0,0 @@
-package ru.hpclab.hl.module1.controller.exeption;
-
-public class LessonException extends RuntimeException {
-
-    public LessonException(String message) {
-        super(message);
-    }
-}
--- highly_loaded_systems-feature-spring-boot-test (2)/highly_loaded_systems-feature-spring-boot-test/src/main/java/ru/hpclab/hl/module1/controller/exeption/ProgressException.java
+++
@@ -1,8 +0,0 @@
-package ru.hpclab.hl.module1.controller.exeption;
-
-public class ProgressException extends RuntimeException {
-
-    public ProgressException(String message) {
-        super(message);
-    }
-}
